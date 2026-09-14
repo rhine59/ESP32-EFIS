472374762635e89f3555dc24bbbf86efd0e68f41
Richard Hine <rhine59@gmail.com>
2026-09-14 08:42:22 +0100

    Add MCP23008 firmware driver

---
+++ firmware/main/mcp23008.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "esp_err.h"
+
+esp_err_t mcp23008_init(void);
+esp_err_t mcp23008_write_gpio(uint8_t value);
+esp_err_t mcp23008_set_output(unsigned bit, bool high);
+uint8_t mcp23008_output_shadow(void);
