75910eae9b3e0def9f6a5f12f1825d135cf3aa09
Richard Hine <rhine59@gmail.com>
2026-09-14 08:47:21 +0100

    Add BMI088 driver interface

---
+++ firmware/main/bmi088.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdint.h>
+#include "esp_err.h"
+
+typedef struct {
+    uint8_t accel_chip_id;
+    uint8_t gyro_chip_id;
+    bool accel_ok;
+    bool gyro_ok;
+} bmi088_status_t;
+
+esp_err_t bmi088_init(bmi088_status_t *status);
