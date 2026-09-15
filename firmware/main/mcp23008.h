e4b44765996ad9d2ebfccbf4c55d54fb50320df5
Richard Hine <rhine59@gmail.com>
2026-09-15 09:24:55 +0100

    Add MCP23008 input reading for panel controls

--- firmware/main/mcp23008.h
+++ firmware/main/mcp23008.h
@@ -7,4 +7,5 @@
 esp_err_t mcp23008_init(void);
 esp_err_t mcp23008_write_gpio(uint8_t value);
 esp_err_t mcp23008_set_output(unsigned bit, bool high);
+esp_err_t mcp23008_read_gpio(uint8_t *value);
 uint8_t mcp23008_output_shadow(void);
